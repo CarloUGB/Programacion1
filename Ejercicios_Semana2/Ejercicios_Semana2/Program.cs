1c2705fa005fb32418aaa2be8a5529bb6a319995
Eduardo ugb <smss061022@ugb.edu.sv>
2022-07-29 15:14:30 -0600

    Ejercicios semana 2, versión 2, los 5 ejercicios hechos

--- Ejercicios_Semana2/Ejercicios_Semana2/Program.cs
+++ Ejercicios_Semana2/Ejercicios_Semana2/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleApp1
             int horas;
             int minutos;
             int segundos;
+
             double b;
             double h;
             double area;
@@ -16,12 +17,25 @@ namespace ConsoleApp1
             double lado2;
             double lado3;
             double perimetro;
+
             double pi = 3.1416;
             double radio;
             double h_cilindro;
             double area_cilindro;
             double volumen_cilindrdo;
 
+            double valor_1;
+            double valor_2;
+            double suma;
+            double resta;
+            double multiplicacion;
+            double division;
+
+            string nombre;
+            int edad;
+            double peso;
+            double estatura;
+            string tipo_de_sangre;
             Console.Write("Ejercicio 1" + "\n");
 
             Console.Write("Ingrese el número de horas: ");
@@ -61,6 +75,62 @@ namespace ConsoleApp1
             Console.Write("El área  del cilindro es de: " + area_cilindro + "\n");
             Console.Write("El volumen  del cilindro es de: " + volumen_cilindrdo + "\n");
 
+            Console.Write("\n" + "Ejercicio 4" + "\n");
+
+            Console.Write("Vamos a sumar"+ "\n");
+            Console.Write("Ingrese el primer valor: ");
+            valor_1 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese el segundo valor: ");
+            valor_2 = double.Parse(Console.ReadLine());
+            suma = valor_1 + valor_2;
+            Console.Write("La respuesta es:"+ suma);
+
+            Console.Write("\n"+"Vamos a restar" + "\n");
+            Console.Write("Ingrese el primer valor: ");
+            valor_1 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese el segundo valor: ");
+            valor_2 = double.Parse(Console.ReadLine());
+            resta = valor_1 - valor_2;
+            Console.Write("La respuesta es:" + resta);
+
+            Console.Write("\n" + "Vamos a multiplicar" + "\n");
+            Console.Write("Ingrese el primer valor: ");
+            valor_1 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese el segundo valor: ");
+            valor_2 = double.Parse(Console.ReadLine());
+            multiplicacion = valor_1 * valor_2;
+            Console.Write("La respuesta es:" + multiplicacion);
+
+            Console.Write("\n" + "Vamos a dividir" + "\n");
+            Console.Write("Ingrese el primer valor: ");
+            valor_1 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese el segundo valor: ");
+            valor_2 = double.Parse(Console.ReadLine());
+            division = valor_1 / valor_2;
+            Console.Write("La respuesta es:" + division);
+
+            Console.Write("\n" + "Ejercicio 5" + "\n");
+
+            Console.Write("Sistema de FOSALUD" + "\n");
+            Console.Write("Ingrese los datos del paciente" + "\n");
+            Console.Write("Nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("Edad: ");
+            edad = int.Parse(Console.ReadLine());
+            Console.Write("Peso(Kg): ");
+            peso = double.Parse(Console.ReadLine());
+            Console.Write("Estatura(m): ");
+            estatura = double.Parse(Console.ReadLine());
+            Console.Write("Tipo de sangre: ");
+            tipo_de_sangre = Console.ReadLine();
+
+            
+            Console.Write("\n"+"LOS DATOS DEL PACIENTE SON:"+ "\n");
+            Console.Write("Nombre: " + nombre + "\n");
+            Console.Write("Edad: "+ edad + "\n");
+            Console.Write("Peso: "+ peso +" kg"+ "\n");
+            Console.Write("Estatura: "+ estatura + " m" + "\n");
+            Console.Write("Tipo de sangre: "+ tipo_de_sangre + "\n");
             Console.Read();
         }
     }
