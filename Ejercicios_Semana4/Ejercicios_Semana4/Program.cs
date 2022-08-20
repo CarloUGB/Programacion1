67e22d99df4b0f409948a8611ab9dcdcc80e7d17
Eduardo ugb <smss061022@ugb.edu.sv>
2022-08-20 16:27:24 -0600

    Utilizando un arreglo bidimensional

--- Ejercicios_Semana4/Ejercicios_Semana4/Program.cs
+++ Ejercicios_Semana4/Ejercicios_Semana4/Program.cs
@@ -8,58 +8,94 @@ namespace Ejercicios_Semana4
         {
             //Bryan Daniel Escobar Umanzor
             //Carlos Eduardo Mejía Luna
-            int elementos;
-            Console.Write("Ingrese el número de registros: ");
-            elementos = int.Parse(Console.ReadLine());
-            string[] nombre = new string[elementos];
-            string[] carrera = new string[elementos];
-            string[] facultad = new string[elementos];
-            int[] edad = new int[elementos];
-            string[] email = new string[elementos];
-            int[] teléfono = new int[elementos];
+            int filas;
+            Console.Write("Ingrese el número de registros (filas): ");
+            filas = int.Parse(Console.ReadLine());
+
+            int columnas;
+            Console.Write("Ingrese el número de datos del registro (Columnas): ");
+            columnas = int.Parse(Console.ReadLine());
+
+            string[,] tabla = new string[filas, columnas];
+
             int registro;
-            string j;
+            string continuar;
 
-            for (int i = 0; i < elementos; i++)
-
+            for (int i = 0; i < filas; i++)
             {
+                Console.WriteLine("_________________________________________");
+                Console.Write("Registro " + i+"\n");
+                Console.WriteLine("_________________________________________");
 
-                Console.Write("Registro " + i + "\n");
-
-                Console.Write("Ingrese su nombre: ");
-                nombre[i] = Console.ReadLine();
-
-
-                Console.Write("Ingrese su carrera: ");
-                carrera[i] = Console.ReadLine();
-
-                Console.Write("Ingrese su facultad: ");
-                facultad[i] = Console.ReadLine();
-
-                Console.Write("Ingrese su edad: ");
-                edad[i] = int.Parse(Console.ReadLine());
-
-                Console.Write("Ingrese su email: ");
-                email[i] = Console.ReadLine();
-
-                Console.Write("Ingrese su teléfono: ");
-                teléfono[i] = int.Parse(Console.ReadLine());
+                for (int j = 0; j < columnas; j++)
+                {
+                    
+                    switch (j)
+                    {
+                        case 0:
+                            Console.Write("Ingrese el nombre: ");
+                            tabla[i,j] = Console.ReadLine();
+                            break;
+                        case 1:
+                            Console.Write("Ingrese la carrera: ");
+                            tabla[i, j] = Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.Write("Ingrese la facultad: ");
+                            tabla[i, j] = Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.Write("Ingrese la edad: ");
+                            tabla[i, j] = Console.ReadLine();
+                            break;
+                        case 4:
+                            Console.Write("Ingrese la dirección email: ");
+                            tabla[i, j] = Console.ReadLine();
+                            break;
+                        case 5:
+                            Console.Write("Ingrese el número de télfeno: ");
+                            tabla[i, j] = Console.ReadLine();
+                            break;
+                    }
+                    Console.WriteLine("_________________________________________");
+                }
             }
-
-            j = "s";
-            while (j == "s")
+            continuar = "s";
+            while (continuar == "s")
             {
-                Console.Write("Ingrese el número de registro que desea consultar: ");
+                Console.Write("Ingrese el número de registro que desea consultar (fila): ");
                 registro = int.Parse(Console.ReadLine());
-                Console.Write("El nombre es: " + nombre[registro] + "\n");
-                Console.Write("La carrera es: " + carrera[registro] + "\n");
-                Console.Write("La facultad es: " + facultad[registro] + "\n");
-                Console.Write("La edad es: " + edad[registro] + "\n");
-                Console.Write("El email es: " + email[registro] + "\n");
-              
-                Console.Write("¿Desea hacer otra consula? Sí o No (S/N): ");
-                j = Console.ReadLine();
+                
+                for (int k = 0; k < columnas; k++)
+                {
+                    switch (k)
+                    {
+                        case 0:
+                            Console.Write("El nombre es: " +" | "+ tabla[registro, k] + "\n");
+                            break;
+                        case 1:
+                            Console.Write("La carrera es: " + " | " + tabla[registro, k] + "\n");
+                            break;
+                        case 2:
+                            Console.Write("La facultad es: " + " | " + tabla[registro, k] + "\n");
+                            break;
+                        case 3:
+                            Console.Write("La edad es: " + " | " + tabla[registro, k] + "\n");
+                            break;
+                        case 4:
+                            Console.Write("La dirección email es: " + " | " + tabla[registro, k] + "\n");
+                            break;
+                        case 5:
+                            Console.Write("El número de télefono es: " + " | " + tabla[registro, k] + "\n");
+                            break;
+                    }
+                    Console.WriteLine("_________________________________________");
+                }
+                Console.Write("¿Desea consultar otro registro? Sí o No (S/N): ");
+                continuar = Console.ReadLine();
+
             }
+
             Console.Write("¡EJECUCIÓN FINALLIZADA!");
             Console.ReadLine();
 
